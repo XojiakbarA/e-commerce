99c6a3a526feeab2efc470fa571dd0bfc5dc84ae
XojiakbarA <akromov.x.1995@gmail.com>
2021-11-27 23:25:27 +0500

    improved list view and actions of ProductCard

--- components/common/ProductCard/index.js
+++ components/common/ProductCard/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {Card, CardContent, CardMedia,
         CardActionArea, Box, Typography,
         IconButton, Tooltip, Rating} from '@mui/material'
@@ -12,17 +13,15 @@ import { productImageURL } from '../../../utils/utils'
 import Link from 'next/link'
 
 const grid = {
-    card: {boxShadow: 3, borderRadius: 2},
+    card: {boxShadow: 3, borderRadius: 2, position: 'relative'},
     cardActionArea: null,
-    cardMedia: null,
-    cardActions: null,
+    cardMedia: null
 }
 
 const list = {
-    card: {boxShadow: 3, borderRadius: 2, display: 'flex', justifyContent: 'flex-start'},
+    card: {boxShadow: 3, borderRadius: 2, display: 'flex', justifyContent: 'flex-start', position: 'relative'},
     cardActionArea: {display: 'flex', justifyContent: 'flex-start'},
-    cardMedia: {width: 200},
-    cardActions: {display: 'flex', flexDirection: 'column', alignItems: 'center'}
+    cardMedia: {width: 200, alignSelf: 'start'}
 }
 
 const ProductCard = ({product, view}) => {
@@ -31,11 +30,34 @@ const ProductCard = ({product, view}) => {
         product.image = {id: null, src: 'no_image.jpeg'}
     }
 
+    const [display, setDisplay] = useState('none')
+    const [ripple, setRipple] = useState(false)
+
+    function handleCardEnter(e) {
+        setDisplay('flex')
+    }
+    function handleCardLeave(e) {
+        setDisplay('none')
+    }
+    function handleActionEnter(e) {
+        setRipple(true)
+    }
+    function handleActionLeave(e) {
+        setRipple(false)
+    }
+    function handleWishlistClick(e) {
+        
+    }
+
     return (
-        <Card sx={ view == 'grid' || view == undefined ? grid.card : list.card }>
-            <CardActionArea sx={ view == 'grid' || view == undefined ? grid.cardActionArea : list.cardActionArea }>
-                <Link href={'/products/' + product.id} >
-                <a>
+        <Card
+            sx={ view == 'grid' || view == undefined ? grid.card : list.card }
+            onMouseEnter={ (e) => handleCardEnter(e) }
+            onMouseLeave={ (e) => handleCardLeave(e) }
+        >
+            <CardActionArea disableRipple={ripple}>
+                <Link href={'/products/' + product.id}>
+                <a style={ view == 'grid' || view == undefined ? grid.cardActionArea : list.cardActionArea }>
                 <CardMedia
                     component="img"
                     sx={ view == 'grid' || view == undefined ? grid.cardMedia : list.cardMedia }
@@ -63,19 +85,23 @@ const ProductCard = ({product, view}) => {
                 </a>
                 </Link>
             </CardActionArea>
-            <Box sx={ view == 'grid' || view == undefined ? grid.cardActions : list.cardActions }>
-                <IconButton>
-                    <Tooltip title='Add to wishlist'>
+            <Box
+                sx={{display: 'flex', flexDirection: 'column', position: 'absolute', top: 0, right: 0}}
+                onMouseEnter={ (e) => handleActionEnter(e) }
+                onMouseLeave={ (e) => handleActionLeave(e) }
+            >
+                <IconButton onClick={ (e) => handleWishlistClick(e) }>
+                    <Tooltip title='Add to wishlist' placement='right'>
                         <FavoriteBorderIcon />
                     </Tooltip>
                 </IconButton>
                 <IconButton>
-                    <Tooltip title='Add to cart'>
+                    <Tooltip title='Add to cart' placement='right'>
                         <ShoppingCartOutlinedIcon />
                     </Tooltip>
                 </IconButton>
                 <IconButton>
-                    <Tooltip title='View'>
+                    <Tooltip title='View' placement='right'>
                         <VisibilityOutlinedIcon />
                     </Tooltip>
                 </IconButton>
