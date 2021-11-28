f2eefa65ec743f1ea6f849c37a978120f791c040
XojiakbarA <akromov.x.1995@gmail.com>
2021-11-28 16:43:57 +0500

    validated login form with formik and yup

--- components/Layout/TopHeader/LoginDialog/index.js
+++ components/Layout/TopHeader/LoginDialog/index.js
@@ -1,8 +1,12 @@
-import { Stack, Button, Dialog, TextField, Typography, Box } from '@mui/material'
+import { Stack, Button, Dialog, TextField, Typography, Box, IconButton } from '@mui/material'
 import Link from 'next/link'
+import ReportIcon from '@mui/icons-material/Report'
+import CloseIcon from '@mui/icons-material/Close'
 import { closeLoginDialog } from '../../../../redux/actions/main'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
 
 const LoginDialog = () => {
 
@@ -12,23 +16,67 @@ const LoginDialog = () => {
 
     const closeDialog = () => dispatch(closeLoginDialog())
 
+    const validationSchema = yup.object({
+        email: yup
+            .string('Enter your email')
+            .email('Enter a valid email')
+            .required('Email is required'),
+        password: yup
+            .string('Enter your password')
+            .min(8, 'Password should be of minimum 8 characters length')
+            .required('Password is required')
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values))
+        }
+    })
+
     return(
         <Dialog open={isOpen} onClose={ closeDialog }>
-            <Box sx={{marginX: {xs: 3, sm: 10}, marginY: {xs: 3, sm: 7}}}>
+            <Box sx={{marginX: {xs: 3, sm: 10}, marginY: {xs: 3, sm: 7}, width: {xs: 250, sm: 300}}}>
+                <IconButton sx={{ position: 'absolute', top: 8, right: 8 }} onClick={ closeDialog }>
+                    <CloseIcon />
+                </IconButton>
                 <Typography variant='h5' textAlign='center' paddingBottom={5}>
                     Welcome to e-commerce
                 </Typography>
-                <Stack spacing={3}>
-                    <TextField label='Email' size='small' />
-                    <TextField label='Password' size='small' type='password' />
-                    <Button variant='contained'>Login</Button>
-                    <Typography variant='body1' textAlign='center'>
-                        Don`t have Account? <Link href='/signup'><a style={{textDecoration: 'underline'}}> Sign Up </a></Link>
-                    </Typography>
-                    <Typography variant='body1' textAlign='center'>
-                        Forgot your password? <Link href='/resetit'><a style={{textDecoration: 'underline'}}> Reset It </a></Link>
-                    </Typography>
-                </Stack>
+
+                <form onSubmit={formik.handleSubmit} >
+                    <Stack spacing={3}>
+
+                        <TextField
+                            label='Email'
+                            size='small'
+                            error={ formik.touched.email && Boolean(formik.errors.email) }
+                            helperText={ formik.touched.email && formik.errors.email }
+                            { ...formik.getFieldProps('email') }
+                        />
+                        <TextField
+                            label='Password'
+                            size='small'
+                            type='password'
+                            error={ formik.touched.password && Boolean(formik.errors.password) }
+                            helperText={ formik.touched.password && formik.errors.password }
+                            { ...formik.getFieldProps('password') }
+                        />
+                        <Button variant='contained' type='submit'>Login</Button>
+
+                        <Typography variant='body1' textAlign='center'>
+                            Don`t have Account? <Link href='/signup'><a style={{textDecoration: 'underline'}}> Sign Up </a></Link>
+                        </Typography>
+                        <Typography variant='body1' textAlign='center'>
+                            Forgot your password? <Link href='/resetit'><a style={{textDecoration: 'underline'}}> Reset It </a></Link>
+                        </Typography>
+                    </Stack>
+                </form>
+
             </Box>
         </Dialog>
     )
