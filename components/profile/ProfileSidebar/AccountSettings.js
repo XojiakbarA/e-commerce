81bafb7707617ea359aa6a4b050c1fbab3df90d7
XojiakbarA <akromov.x.1995@gmail.com>
2021-12-02 02:00:54 +0500

    added active state for list

--- components/profile/ProfileSidebar/AccountSettings.js
+++ components/profile/ProfileSidebar/AccountSettings.js
@@ -1,24 +1,33 @@
 import { List, ListSubheader, ListItemButton, ListItemText, ListItemIcon } from "@mui/material"
+import Link from '../../common/Link'
 import PersonIcon from '@mui/icons-material/Person'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import PaymentIcon from '@mui/icons-material/Payment'
+import { useRouter } from "next/dist/client/router"
+
+const list = [
+    {title: 'Profile Info', path: '/profile', icon: (<PersonIcon/>)},
+    {title: 'Addresses', path: '/profile/addresses', icon: (<LocationOnIcon/>)},
+    {title: 'Payment Methods', path: 'profile/payment-methods', icon: (<PaymentIcon/>)}
+]
 
 const AccountSettings = () => {
+
+    const router = useRouter()
+
     return (
         <List>
             <ListSubheader>Account Settings</ListSubheader>
-            <ListItemButton>
-                <ListItemIcon><PersonIcon/></ListItemIcon>
-                <ListItemText primary='Profile Info'/>
-            </ListItemButton>
-            <ListItemButton>
-                <ListItemIcon><LocationOnIcon/></ListItemIcon>
-                <ListItemText primary='Addresses'/>
-            </ListItemButton>
-            <ListItemButton>
-            <ListItemIcon><PaymentIcon/></ListItemIcon>
-            <ListItemText primary='Payment Methods'/>
-            </ListItemButton>
+            {
+                list.map((item, i) => (
+                    <Link key={i} href={item.path}>
+                        <ListItemButton key={i} selected={item.path == router.pathname}>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.title}/>
+                        </ListItemButton>
+                    </Link>
+                ))
+            }
         </List>
     )
 }
--- components/profile/ProfileSidebar/Dashboard.js
+++ components/profile/ProfileSidebar/Dashboard.js
@@ -1,24 +1,33 @@
 import { List, ListSubheader, ListItemButton, ListItemText, ListItemIcon } from "@mui/material"
+import Link from '../../common/Link'
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import HeadsetMicIcon from '@mui/icons-material/HeadsetMic'
+import { useRouter } from "next/dist/client/router"
+
+const list = [
+    {title: 'Orders', path: '/profile/orders', icon: (<ShoppingBagIcon/>)},
+    {title: 'Wishlist', path: '/wishlist', icon: (<FavoriteIcon/>)},
+    {title: 'Support Tickets', path: 'profile/support-tickets', icon: (<HeadsetMicIcon/>)}
+]
 
 const Dashboard = () => {
+
+    const router = useRouter()
+
     return (
         <List>
             <ListSubheader>Dashboard</ListSubheader>
-            <ListItemButton>
-                <ListItemIcon><ShoppingBagIcon/></ListItemIcon>
-                <ListItemText primary='Orders'/>
-            </ListItemButton>
-            <ListItemButton>
-                <ListItemIcon><FavoriteIcon/></ListItemIcon>
-                <ListItemText primary='Wishlist'/>
-            </ListItemButton>
-            <ListItemButton>
-            <ListItemIcon><HeadsetMicIcon/></ListItemIcon>
-            <ListItemText primary='Support Tickets'/>
-            </ListItemButton>
+            {
+                list.map((item, i) => (
+                    <Link key={i} href={item.path}>
+                        <ListItemButton key={i} selected={item.path == router.pathname}>
+                            <ListItemIcon>{item.icon}</ListItemIcon>
+                            <ListItemText primary={item.title}/>
+                        </ListItemButton>
+                    </Link>
+                ))
+            }
         </List>
     )
 }
