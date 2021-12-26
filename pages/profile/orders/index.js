e89412fd6e4e6af7f1a0002a87b5ecaaf4744d42
XojiakbarA <akromov.x.1995@gmail.com>
2021-12-26 17:10:56 +0500

    moved orders page to orders directory

--- pages/profile/orders/index.js
+++ pages/profile/orders/index.js
@@ -1,8 +1,8 @@
-import ProfileLayout from "../../components/layout/ProfileLayout";
-import ProfileTitle from "../../components/profile/ProfileTitle";
+import ProfileLayout from "../../../components/layout/ProfileLayout";
+import ProfileTitle from "../../../components/profile/ProfileTitle";
 
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
-import OrderList from "../../components/profile/OrderList/OrderList";
+import OrderList from "../../../components/profile/OrderList/OrderList";
 
 const Orders = () => {
     return (
