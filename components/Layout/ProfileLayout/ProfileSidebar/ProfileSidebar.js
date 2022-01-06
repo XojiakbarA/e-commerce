e216b05d354022046b5736775046f4794ff85f92
XojiakbarA <akromov.x.1995@gmail.com>
2022-01-06 23:53:47 +0500

    added to menu create shop link

--- components/Layout/ProfileLayout/ProfileSidebar/ProfileSidebar.js
+++ components/Layout/ProfileLayout/ProfileSidebar/ProfileSidebar.js
@@ -1,4 +1,5 @@
 import { Grid, Paper, Stack, Divider, List, ListSubheader, ListItemButton, ListItemText, ListItemIcon } from "@mui/material"
+import AddBusinessIcon from '@mui/icons-material/AddBusiness'
 import NextLink from '../../../common/Link'
 import { useRouter } from "next/dist/client/router"
 
@@ -29,6 +30,16 @@ const ProfileSidebar = ({menu}) => {
                                 </NextLink>
                             ))
                         }
+                        <NextLink href={'/profile/create-shop'}>
+                            <ListItemButton
+                                selected={router.pathname == '/profile/create-shop'}
+                            >
+                                <ListItemIcon>
+                                    <AddBusinessIcon/>
+                                </ListItemIcon>
+                                <ListItemText primary='Create Shop'/>
+                            </ListItemButton>
+                        </NextLink>
                     </List>
                 </Stack>
             </Paper>
