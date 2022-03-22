586eec24c30ca51962e535dbce682802e2374f70
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-03-22 09:50:20 +0500

    updated carousel package

--- components/index/Banner/index.js
+++ components/index/Banner/index.js
@@ -1,6 +1,5 @@
 import Carousel from 'react-material-ui-carousel'
 import BannerItem from './BannerItem'
-import style from './Banner.module.css'
 import { useSelector } from 'react-redux'
 
 const Banner = () => {
@@ -8,7 +7,7 @@ const Banner = () => {
     const banners = useSelector(state => state.banners)
 
     return (
-        <Carousel className={style.wrapper}>
+        <Carousel sx={{ overflow: 'visible' }}>
             {
                 banners.map( (banner) => <BannerItem key={banner.id} banner={banner} /> )
             }
