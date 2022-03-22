62f507da234b0ac3237aed2ad4b58e59ccea8684
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-03-22 14:37:05 +0500

    fixed position of carousel

--- components/index/Banner/BannerItem.js
+++ components/index/Banner/BannerItem.js
@@ -1,59 +1,39 @@
-import { Button, Grid, Box, Typography, Stack } from '@mui/material'
+import { Button, Grid, Box, Typography, Stack, Card } from '@mui/material'
 import Image from 'next/image'
 import { bannerImageURL } from '../../../utils/utils'
 
-const styles = {
-    container: {
-        boxShadow: 3,
-        borderRadius: 2,
-        backgroundColor: 'white',
-        position: 'relative', 
-        overflow: 'hidden'
-    },
-    leftItem: {
-        padding: 2,
-        display: 'flex',
-        justifyContent: 'start',
-        alignItems: 'center',
-        position: {xs: 'absolute', sm: 'static'},
-        height: {xs: '100%', sm: 'auto'},
-        zIndex: 1,
-        backgroundColor: {xs: 'rgba(117, 117, 117, 0.3)', sm: 'transparent'}
-    },
-    rightItem: {
-        padding: 2,
-        display: {sm: 'block'}
-    }
-}
-
 const BannerItem = ({banner}) => {
     return (
-        <Grid container spacing={0} sx={styles.container}>
-            <Grid item lg={6} sx={styles.leftItem}>
-                <Stack spacing={2}>
-                    <Typography variant='h4'>
-                        {banner.title}
-                    </Typography>
-                    <Typography variant='body2'>
-                        {banner.description}
-                    </Typography>
-                    <Box>
-                        <Button variant='contained'>
-                            Check it out!
-                        </Button>
+        <Card elevation={5}>
+            <Grid container spacing={0} padding={2}>
+                <Grid item lg={6} display='flex' alignItems='center'>
+                    <Stack spacing={2}>
+                        <Typography variant='h4'>
+                            {banner.title}
+                        </Typography>
+                        <Typography variant='body2'>
+                            {banner.description}
+                        </Typography>
+                        <Box>
+                            <Button variant='contained'>
+                                Check it out!
+                            </Button>
+                        </Box>
+                    </Stack>
+                </Grid>
+                <Grid item lg={6}>
+                    <Box sx={{ position: 'relative', width: 450, height: 450 }}>
+                        <Image
+                            src={bannerImageURL + banner.image}
+                            alt={banner.image}
+                            layout='fill'
+                            objectFit='cover'
+                            priority
+                        />
                     </Box>
-                </Stack>
+                </Grid>
             </Grid>
-            <Grid item lg={6} sx={styles.rightItem}>
-                <Image
-                    src={bannerImageURL + banner.image}
-                    alt={banner.image}
-                    width={450}
-                    height={450}
-                    priority
-                />
-            </Grid>
-        </Grid>
+        </Card>
     )
 }
 
