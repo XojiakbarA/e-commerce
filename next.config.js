88b9528ed15d861d6920f326999578d65d659cac
Xojiakbar <87039534+XojiakbarA@users.noreply.github.com>
2022-04-09 02:19:33 +0500

    installed next-bundle-analyzer package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['127.0.0.1']
+//   }
+// }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ['127.0.0.1']
   }
-}
+})
